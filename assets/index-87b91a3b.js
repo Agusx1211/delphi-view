7f0d47e3f7ab6cdf1564b675a682893f333e1dec
Agusx1211 <agusxrun@gmail.com>
2024-07-15 13:11:53 +0000

    Build: (2ec3c6c) Merge pull request #3 from nachopiris/master
    deploy / migrate wallet feature and replace mumbai deprecated network…

---
+++ assets/index-87b91a3b.js
@@ -0,0 +1,9 @@
+import{dy as o,d3 as r,du as t,dF as i,dH as d,dI as n,dJ as f,dE as c,dz as l,dD as h,bF as m,dC as p,q as I,bK as w,N as x,dG as E,dA as H,dB as V}from"./index-8a7a387b.js";const a=`
+@font-face {
+  font-family: 'Inter';
+  font-style:  normal;
+  font-weight: 300 600;
+  font-display: swap;
+  src: url("https://rsms.me/inter/font-files/InterVariable.woff2") format("woff2-variations");
+}
+`;export{a as InterVar,o as ProviderRpcError,r as ProviderRpcErrorCode,t as bigIntToHex,i as chainIdToViemImport,d as chainIdValidation,n as chainNamespaceValidation,f as chainValidation,c as createDownloadMessage,l as createEIP1193Provider,h as ethToWeiBigInt,m as fromHex,p as isAddress,I as isHex,w as parseEther,x as toHex,E as validate,H as weiHexToEth,V as weiToEth};
