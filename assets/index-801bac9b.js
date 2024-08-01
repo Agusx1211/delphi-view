03fe32f05396bf95b78808bd422ec8185c02f6b6
Agusx1211 <agusxrun@gmail.com>
2024-08-01 09:14:30 +0000

    Build: (e01aa75) Merge pull request #4 from nachopiris/master
    recover signature

--- assets/index-801bac9b.js
+++ assets/index-801bac9b.js
@@ -1,4 +1,4 @@
-import{dy as o,d3 as r,du as t,dF as i,dH as d,dI as n,dJ as f,dE as c,dz as l,dD as h,bF as m,dC as p,q as I,bK as w,N as x,dG as E,dA as H,dB as V}from"./index-8a7a387b.js";const a=`
+import{dy as o,d3 as r,du as t,dF as i,dH as d,dI as n,dJ as f,dE as c,dz as l,dD as h,bF as m,dC as p,q as I,bK as w,N as x,dG as E,dA as H,dB as V}from"./index-8ff2e4a7.js";const a=`
 @font-face {
   font-family: 'Inter';
   font-style:  normal;
