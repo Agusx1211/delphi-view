412356ab6006facc6c41db0ab4be7740c829afaa
Michael Standen <mstan@horizon.io>
2024-09-05 09:09:20 +1200

    List all supported networks

--- src/stores/NetworksStore.ts
+++ src/stores/NetworksStore.ts
@@ -1,93 +1,30 @@
-import { NetworkConfig, ChainId, networks } from '@0xsequence/network'
+import { NetworkConfig, ChainId, networks, NetworkMetadata } from '@0xsequence/network'
 
 export const indexerURL = (network: string) => `https://${network}-relayer.sequence.app`
 export const relayerURL = (network: string) => `https://${network}-indexer.sequence.app`
 export const nodesURL = (network: string) => `https://nodes.sequence.app/${network}`
 
-const genUrls = (network: string) => {
-  const rpcUrl = nodesURL(network)
+const genUrls = (networkName: string) => {
+  const rpcUrl = nodesURL(networkName)
   return {
     rpcUrl,
     relayer: {
-      url: relayerURL(network),
+      url: relayerURL(networkName),
       provider: {
         url: rpcUrl,
       }
     },
-    indexerUrl: indexerURL(network)
+    indexerUrl: indexerURL(networkName)
   }
 }
 
+const genNetworkConfig: (network: NetworkMetadata) => NetworkConfig = (network) => ({
+  ...network,
+  ...genUrls(network.name),
+  isDefaultChain: network.chainId === ChainId.MAINNET,
+})
+
 // Add default values for API, relayer and Indexer
-export const NETWORKS: NetworkConfig[] = [
-  {
-    ...networks[ChainId.MAINNET],
-    ...genUrls('mainnet'),
-    isDefaultChain: true
-  },
-  {
-    ...networks[ChainId.SEPOLIA],
-    ...genUrls('sepolia'),
-  },
-  {
-    ...networks[ChainId.POLYGON],
-    ...genUrls('polygon'),
-  },
-  {
-    ...networks[ChainId.BSC],
-    ...genUrls('bsc'),
-  },
-  {
-    ...networks[ChainId.AVALANCHE],
-    ...genUrls('avalanche'),
-  },
-  {
-    ...networks[ChainId.ARBITRUM],
-    ...genUrls('arbitrum'),
-  },
-  {
-    ...networks[ChainId.ARBITRUM_NOVA],
-    ...genUrls('arbitrum-nova'),
-  },
-  {
-    ...networks[ChainId.ARBITRUM_SEPOLIA],
-    ...genUrls('arbitrum-sepolia'),
-  },
-  {
-    ...networks[ChainId.OPTIMISM],
-    ...genUrls('optimism'),
-  },
-  {
-    ...networks[ChainId.POLYGON_ZKEVM],
-    ...genUrls('polygon-zkevm'),
-    name: 'Pol-ZKEVM'
-  },
-  {
-    ...networks[ChainId.GNOSIS],
-    ...genUrls('gnosis'),
-  },
-  {
-    ...networks[ChainId.POLYGON_AMOY],
-    ...genUrls('amoy'),
-  },
-  {
-    ...networks[ChainId.BSC_TESTNET],
-    ...genUrls('bsc-testnet'),
-  },
-  {
-    ...networks[ChainId.BASE],
-    ...genUrls('base'),
-  },
-  {
-    ...networks[ChainId.HOMEVERSE],
-    ...genUrls('homeverse'),
-  },
-  {
-    ...networks[ChainId.HOMEVERSE_TESTNET],
-    ...genUrls('homeverse-testnet'),
-  },
-  {
-    ...networks[ChainId.AVALANCHE_TESTNET],
-    ...genUrls('avalanche-testnet'),
-  }
-]
+export const NETWORKS: NetworkConfig[] = Object.values(networks)
+  .filter(n => !n.deprecated)
+  .map(n => genNetworkConfig(n))
