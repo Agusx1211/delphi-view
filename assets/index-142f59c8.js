951ca4e36ec25241e5059015685aa0c0b00e2d99
Agusx1211 <agusxrun@gmail.com>
2024-12-02 14:06:34 +0000

    Build: (186a8d6) Merge pull request #9 from Agusx1211/revert-8-master
    Revert "Update to Sequence V2 for last chains and add ApiKey for node requests"

--- assets/index-142f59c8.js
+++
@@ -1,9 +0,0 @@
-import{ds as o,cZ as r,dn as t,dz as i,dB as d,dC as n,dD as f,dy as c,dt as l,dx as h,bz as m,dw as p,q as I,bE as w,O as x,dA as E,du as v,dv as V}from"./index-450029f9.js";const a=`
-@font-face {
-  font-family: 'Inter';
-  font-style:  normal;
-  font-weight: 300 600;
-  font-display: swap;
-  src: url("https://rsms.me/inter/font-files/InterVariable.woff2") format("woff2-variations");
-}
-`;export{a as InterVar,o as ProviderRpcError,r as ProviderRpcErrorCode,t as bigIntToHex,i as chainIdToViemImport,d as chainIdValidation,n as chainNamespaceValidation,f as chainValidation,c as createDownloadMessage,l as createEIP1193Provider,h as ethToWeiBigInt,m as fromHex,p as isAddress,I as isHex,w as parseEther,x as toHex,E as validate,v as weiHexToEth,V as weiToEth};
